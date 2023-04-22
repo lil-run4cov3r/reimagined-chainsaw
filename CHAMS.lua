2246723377e67cb8a3314ebc9724c4b56d3db1f7
lil-run4cov3r <101074436+lil-run4cov3r@users.noreply.github.com>
2023-04-21 22:12:51 -0500

    Update CHAMS.lua

--- CHAMS.lua
+++ CHAMS.lua
@@ -1,42 +1,64 @@
-local Players = game:GetService("Players"):GetChildren()
-local RunService = game:GetService("RunService")
-local highlight = Instance.new("Highlight")
-highlight.Name = "Highlight"
+local Players = game:GetService("Players")
+local StarterPlayer = game:GetService("StarterPlayer")
+local Highlight = Instance.new("Highlight")
+Highlight.Name = "Highlight"
+Highlight.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+StarterPlayer:WaitForChild("StarterCharacterScripts")
 
-local isHighlightEnabled = false
-
-local function updateHighlightEffect()
-    for i, v in pairs(Players) do
-        repeat wait() until v.Character
-        if not v.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight") then
-            local highlightClone = highlight:Clone()
-            highlightClone.Adornee = v.Character
-            highlightClone.Parent = v.Character:FindFirstChild("HumanoidRootPart")
-            highlightClone.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
-            highlightClone.Name = "Highlight"
-        end
-    end
+function createHighlight(player)
+    local character = player.Character or player.CharacterAdded:Wait()
+    local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+    local highlightClone = Highlight:Clone()
+    highlightClone.Adornee = humanoidRootPart
+    highlightClone.Parent = humanoidRootPart
+    highlightClone.Name = "Highlight"
 end
 
-local function removeHighlightEffect()
-    for i, v in pairs(Players) do
-        if v.Character and v.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight") then
-            v.Character:FindFirstChild("HumanoidRootPart").Highlight:Destroy()
-        end
-    end
+for _, player in ipairs(Players:GetPlayers()) do
+    createHighlight(player)
 end
 
-local function toggleHighlight()
-    isHighlightEnabled = not isHighlightEnabled
-    if isHighlightEnabled then
-        updateHighlightEffect()
-    else
-        removeHighlightEffect()
+Players.PlayerAdded:Connect(function(player)
+    createHighlight(player)
+end)
+
+Players.PlayerRemoving:Connect(function(player)
+    local character = player.Character
+    if character then
+        local humanoidRootPart = character:FindFirstChild("HumanoidRootPart")
+        if humanoidRootPart then
+            local highlight = humanoidRootPart:FindFirstChild("Highlight")
+            if highlight then
+                highlight:Destroy()
+            end
+        end
+    end
+end)
+
+function toggleESP(value)
+    -- Toggle the ESP effect here
+end
+
+function toggleHighlight(value)
+    for _, player in ipairs(Players:GetPlayers()) do
+        local character = player.Character
+        if character then
+            local humanoidRootPart = character:FindFirstChild("HumanoidRootPart")
+            if humanoidRootPart then
+                local highlight = humanoidRootPart:FindFirstChild("Highlight")
+                if highlight then
+                    highlight.Enabled = value
+                end
+            end
+        end
     end
 end
 
 OthersTab:AddToggle({
-    Name = "Highlight Players",
+    Name = "ESP",
     Default = false,
-    Callback = toggleHighlight,
+    Callback = function(value)
+        toggleESP(value)
+        toggleHighlight(value) -- Toggle the highlight effect along with the ESP effect
+    end,
 })
