4bfc61bc1949c37ba1c8db549946b4a7c92d49e3
lil-run4cov3r <101074436+lil-run4cov3r@users.noreply.github.com>
2023-04-21 21:04:42 -0500

    Update CHAMS.lua

--- CHAMS.lua
+++ CHAMS.lua
@@ -28,16 +28,5 @@ game.Players.PlayerRemoving:Connect(function(playerRemoved)
     playerRemoved.Character:FindFirstChild("HumanoidRootPart").Highlight:Destroy()
 end)
 
-RunService.Heartbeat:Connect(function()
-    for i, v in pairs(Players) do
-        repeat wait() until v.Character
-        if not v.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight") then
-            local highlightClone = highlight:Clone()
-            highlightClone.Adornee = v.Character
-            highlightClone.Parent = v.Character:FindFirstChild("HumanoidRootPart")
-            highlightClone.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
-            highlightClone.Name = "Highlight"
-            task.wait()
-        end
 end
 end)
